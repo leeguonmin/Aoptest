e0c236e750289c02c8bc7e3d7f171faeed359fd2
lee <l_lpp@naver.com>
2025-01-20 16:02:04 +0900

    전달된 인자

--- src/main/java/himedia/aoptest/MyAspect.java
+++ src/main/java/himedia/aoptest/MyAspect.java
@@ -1,10 +1,22 @@
 package himedia.aoptest;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyAspect {
+	@Before("execution(public ProductVo himedia.aoptest.ProductService.findProduct(..))")
+	public void before(JoinPoint joinPoint) {
+		System.out.println("call [defore dadvice]");
+		System.out.println("메서드 실행 전:" + joinPoint.getSignature().getName());
+		
+		Object[] args = joinPoint.getArgs();			// 메서드로 전달된 데이터 객체
+		if (args != null && args.length > 0) {
+			System.out.println("전달된 인자:" + args[0]);
+		}
+	}
 
 }
