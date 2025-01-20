49770679796d5145cba80fcabf42912dcc75c03d
lee <l_lpp@naver.com>
2025-01-20 15:40:43 +0900

    ㅅ서비스를 등록하고....불러오고.....

--- src/main/java/himedia/aoptest/App.java
+++ src/main/java/himedia/aoptest/App.java
@@ -1,10 +1,20 @@
 package himedia.aoptest;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ApplicationContext ac = new ClassPathXmlApplicationContext("config/applicationContext.xml");
+		// 이 과정을 통해 서비스가 등록된거고
+		
+		// 이제 받아올거야 
+		// Spring Context로부터 ProductService Bean 획득
+		ProductService ps = ac.getBean(ProductService.class);
+		
+		ProductVo vo = ps.findProduct("Camera");
+		System.out.println(vo);
 	}
 
 }
