5e1bc980431dd2aea5387173cff94be55476595e
lee <l_lpp@naver.com>
2025-01-20 15:33:59 +0900

    뭐하는건지잘모르겠음

---
+++ src/main/java/himedia/aoptest/App.java
@@ -0,0 +1,10 @@
+package himedia.aoptest;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/himedia/aoptest/ProductService.java
+++ src/main/java/himedia/aoptest/ProductService.java
@@ -1,8 +1,11 @@
 package himedia.aoptest;
 
+import org.springframework.stereotype.Service;
+
 // 원래는 패키지 제대로 나눠서 분리해야하는데
 // 지금은 관련된 특정 지점에 특정 기능을 수행할 수 있도록 만들거라 test 용이라 이렇게하는 거임
 
+@Service
 public class ProductService {
 	public ProductVo findProduct(String keyword) {
 		System.out.println("finding [" + keyword + "] ...");
